adee8f7216c4b1743cf2555fbdd88cdefb6c4990
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-06-29 20:55:16 +0530

    Row #2

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,5 +1,6 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline';
 import { Movie } from '../typings';
+import { Thumbnail } from './';
 
 interface Props {
 	item: {
@@ -16,8 +17,14 @@ const Row = ({ item }: Props) => {
 			</h2>
 			<div className="group relative md:-ml-2">
 				<ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer transition hover:scale-125 group-hover:opacity-100" />
-				<div> </div>
-				<ChevronRightIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer transition hover:scale-125 group-hover:opacity-100" />
+
+				<div className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2">
+					{item.movies.map((movie) => (
+						<Thumbnail key={movie.id} movie={movie} />
+					))}
+				</div>
+
+				<ChevronRightIcon className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
 			</div>
 		</div>
 	);
---
+++ components/Thumbnail.tsx
@@ -0,0 +1,11 @@
+import { Movie } from '../typings';
+
+interface Props {
+	movie: Movie;
+}
+
+function Thumbnail({ movie }: Props) {
+	return <div>Thumbnail</div>;
+}
+
+export default Thumbnail;
