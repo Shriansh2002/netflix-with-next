5d1ad867eda73147cf5e45937940684bbab5862b
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-06-29 23:22:33 +0530

    login page work

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,30 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface Inputs {
+	email: string;
+	password: string;
+}
 
 const login = () => {
 	const [login, setLogin] = useState(false);
 
-	
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Inputs>();
+
+	const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+		if (login) {
+			// await  signIn(email, password)
+		} else {
+			// await signUp(email, password)
+		}
+	};
+
 	return (
 		<div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
 			<Head>
@@ -26,7 +45,10 @@ const login = () => {
 				width={150}
 			/>
 
-			<form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+			<form
+				className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+				onSubmit={handleSubmit(onSubmit)}
+			>
 				<h1 className="text-4xl font-semibold">Sign In</h1>
 
 				<div className="space-y-4">
@@ -35,24 +57,47 @@ const login = () => {
 							type="email"
 							placeholder="Email"
 							className="input"
+							{...register('email', { required: true })}
 						/>
+						{errors.email && (
+							<p className="p-1 text-[13px] font-light text-orange-500">
+								Please Enter a valid Email
+							</p>
+						)}
 					</label>
 					<label className="inline-block w-full">
 						<input
 							type="password"
 							placeholder="Password"
 							className="input"
+							{...register('password', {
+								required: true,
+								minLength: 8,
+								maxLength: 20,
+							})}
 						/>
+						{errors.password && (
+							<p className="p-1 text-[13px] font-light text-orange-500">
+								Your password must contain between 8 and 20
+								characters
+							</p>
+						)}
 					</label>
 				</div>
 
-				<button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+				<button
+					className="w-full rounded bg-[#e50914] py-3 font-semibold"
+					onClick={() => setLogin(true)}
+				>
 					Sign In
 				</button>
 
 				<div className="text-[gray]">
 					New to Netflix?{' '}
-					<button className="text-white hover:underline">
+					<button
+						className="text-white hover:underline"
+						onClick={() => setLogin(false)}
+					>
 						Sign Up Now
 					</button>
 				</div>
