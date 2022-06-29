3b183bb72fd8bbf070ff95da1a1be4ec63f49f84
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-06-30 01:50:12 +0530

    Modal #3

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,15 +1,25 @@
-import { XIcon } from '@heroicons/react/solid';
 import MuiModal from '@mui/material/Modal';
 import { useEffect, useState } from 'react';
+import { FaPlay } from 'react-icons/fa';
+import {
+	CheckIcon,
+	PlusIcon,
+	ThumbUpIcon,
+	VolumeOffIcon,
+	VolumeUpIcon,
+	XIcon,
+} from '@heroicons/react/outline';
+import ReactPlayer from 'react-player';
 import { useRecoilState } from 'recoil';
 import { modalState, movieState } from '../atoms/modalAtom';
-import { Movie, Element } from '../typings';
+import { Movie, Element, Genre } from '../typings';
 
 function Modal() {
 	const [showModal, setShowModal] = useRecoilState(modalState);
 	const [movie, setMovie] = useRecoilState(movieState);
 	const [trailer, setTrailer] = useState('');
 	const [genres, setGenres] = useState<Genre[]>([]);
+	const [muted, setMuted] = useState(true);
 
 	useEffect(() => {
 		if (!movie) return;
@@ -41,7 +51,11 @@ function Modal() {
 	};
 
 	return (
-		<MuiModal open={showModal} onClose={handleClose}>
+		<MuiModal
+			open={showModal}
+			onClose={handleClose}
+			className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+		>
 			<>
 				<button
 					className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
@@ -50,7 +64,43 @@ function Modal() {
 					<XIcon className="h-6 w-6" />
 				</button>
 
-				<div></div>
+				<div className="relative pt-[56.25%]">
+					<ReactPlayer
+						url={`https://www.youtube.com/watch?v=${trailer}`}
+						width="100%"
+						height="100%"
+						style={{ position: 'absolute', top: '0', left: '0' }}
+						playing
+						muted={muted}
+					/>
+
+					<div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+						<div className="flex space-x-2">
+							<button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
+								<FaPlay className="h-7 w-7 text-black" />
+								Play
+							</button>
+
+							<button className="modalButton">
+								<PlusIcon className="h-7 w-7" />
+							</button>
+
+							<button className="modalButton">
+								<ThumbUpIcon className="h-6 w-6" />
+							</button>
+						</div>
+						<button
+							className="modalButton"
+							onClick={() => setMuted((ps) => !ps)}
+						>
+							{muted ? (
+								<VolumeOffIcon className="h-6 w-6" />
+							) : (
+								<VolumeUpIcon className="h-6 w-6" />
+							)}
+						</button>
+					</div>
+				</div>
 			</>
 		</MuiModal>
 	);
